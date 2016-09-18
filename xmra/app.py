0185e2dd86a575dc779874b360f6aa210d0d7c73
z <z@xnz.me>
2016-09-18 17:27:31 -0400

    add resource and refactor json view model as function

--- xmra/app.py
+++ xmra/app.py
@@ -1,8 +1,10 @@
 import falcon
 from xmra.resources.MapPackage import MapPackageResource
+from xmra.resources.MapPackage import MapPackageCollection
 from xmra.middleware import CorsMiddleware
 
 
 api = falcon.API(middleware=[CorsMiddleware()])
 api.req_options.auto_parse_form_urlencoded = True
-api.add_route('/maps', MapPackageResource())
+api.add_route('/maps', MapPackageCollection())
+api.add_route('/map/{map_package_id}', MapPackageResource())
--- xmra/resources/MapPackage.py
+++ xmra/resources/MapPackage.py
@@ -11,69 +11,81 @@ from xmra.util import DateTimeEncoder
 from sqlalchemy import func
 
 
-class MapPackageResource:
+class MapPackageCollection:
 
     def on_get(self, req, resp):
         """Handles GET requests"""
         print(req.params)
 
-        map_packages = []
-
         q = session.query(MapPackage).filter(MapPackage.bsp.any())
-        for mp in q:
-            r_map_package = {
-                'id': mp.map_package_id,
-                'pk3': mp.pk3_file,
-                'bsp': {},
-                'shasum': mp.shasum,
-                'filesize': mp.filesize,
-                'date': mp.date,
-            }
-
-            for bsp in mp.bsp:
-                r_bsp = {
-                    'bsp_file': bsp.bsp_file,
-                    'title': bsp.title,
-                    'license': bsp.license,
-                    'map': bsp.map_file,
-                    'radar': bsp.radar,
-                    'waypoints': bsp.waypoints,
-                    'description': bsp.description,
-                    'mapinfo': bsp.mapinfo,
-                    'entities': {},
-                    'gametypes': [],
-                    'mapshot': bsp.mapshot,
-                    'author': bsp.author
-                }
-                r_map_package['bsp'].update({bsp.bsp_name: r_bsp})
-
-                q_entites = session.query(BspEntity).filter_by(bsp_id=bsp.bsp_id).join(Entity)
-
-                for e in q_entites:
-                    print(e.entity_id)
-                    print(e.bsp_id)
-                    print(e.entity.name)
-
-                    r_entity = {
-                        e.entity.name: e.value
-                    }
-
-                    r_bsp['entities'].update(r_entity)
-
-                q_gametype = session.query(BspGametype).filter_by(bsp_id=bsp.bsp_id).join(Gametype)
-
-                for g in q_gametype:
-                    print(g.gametype_id)
-                    print(g.bsp_id)
-                    print(g.gametype.name)
-
-                    r_bsp['gametypes'].append(g.gametype.name)
-
-            map_packages.append(r_map_package)
-
-        print(map_packages)
+        map_packages = get_map_json(q)
 
         resp.body = json.dumps({'data': map_packages}, cls=ObjectEncoder)
 
     def on_post(self, req, resp):
         pass
+
+
+class MapPackageResource:
+
+    def on_get(self, req, resp, map_package_id):
+        """Handles GET requests"""
+        print(req.params)
+
+        q = session.query(MapPackage).filter_by(map_package_id=map_package_id).all()
+        map_package = get_map_json(q)
+
+        resp.body = json.dumps({'data': map_package}, cls=ObjectEncoder)
+
+    def on_post(self, req, resp):
+        pass
+
+
+def get_map_json(query):
+
+    map_packages = []
+
+    for map_pack in query:
+        r_map_package = {
+            'id': map_pack.map_package_id,
+            'pk3': map_pack.pk3_file,
+            'bsp': {},
+            'shasum': map_pack.shasum,
+            'filesize': map_pack.filesize,
+            'date': map_pack.date,
+        }
+
+        for bsp in map_pack.bsp:
+            r_bsp = {
+                'bsp_file': bsp.bsp_file,
+                'title': bsp.title,
+                'license': bsp.license,
+                'map': bsp.map_file,
+                'radar': bsp.radar,
+                'waypoints': bsp.waypoints,
+                'description': bsp.description,
+                'mapinfo': bsp.mapinfo,
+                'entities': {},
+                'gametypes': [],
+                'mapshot': bsp.mapshot,
+                'author': bsp.author
+            }
+            r_map_package['bsp'].update({bsp.bsp_name: r_bsp})
+
+            q_entites = session.query(BspEntity).filter_by(bsp_id=bsp.bsp_id).join(Entity)
+
+            for ent in q_entites:
+                r_entity = {
+                    ent.entity.name: ent.value
+                }
+
+                r_bsp['entities'].update(r_entity)
+
+            q_gametype = session.query(BspGametype).filter_by(bsp_id=bsp.bsp_id).join(Gametype)
+
+            for gt in q_gametype:
+                r_bsp['gametypes'].append(gt.gametype.name)
+
+        map_packages.append(r_map_package)
+
+    return map_packages
