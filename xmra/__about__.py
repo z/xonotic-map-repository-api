da4f7b5c865a40df33d1aad8850fbdf394cc1ba0
z <z@xnz.me>
2016-10-04 19:02:49 -0400

    add package info

---
+++ xmra/__about__.py
@@ -0,0 +1,24 @@
+__all__ = [
+    '__title__',
+    '__package_name__',
+    '__version__',
+    '__summary__',
+    '__keywords__',
+    '__author__',
+    '__email__',
+    '__author__',
+    '__license__',
+    '__url__',
+    '__copyright__',
+]
+
+__title__ = 'Xonotic Map Repository API'
+__package_name__ = 'xmra'
+__version__ = '0.3.0'
+__summary__ = 'Parses Xonotic map packages into a database and provides a REST API.'
+__keywords__ = ['xonotic', 'xonotic-maps', 'map-repository', 'cli']
+__author__ = 'Tyler Mulligan'
+__email__ = 'z@xnz.me'
+__license__ = 'MIT'
+__url__ = 'https://www.github.com/z/xonotic-map-repository-api'
+__copyright__ = 'Copyright 2016 Tyler Mulligan'
